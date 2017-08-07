942f35d45c993589aba79b53441a4bbf984fcdb5
Mujib517 <mujib517@gmail.com>
2017-08-07 21:52:22 +0530

    Javascript

---
+++ test.js
@@ -0,0 +1,32 @@
+
+var dt = new Date();
+
+console.log(dt.getDate() + " - " + (dt.getMonth() + 1) + " - " + dt.getFullYear());
+
+
+// var firstName = "Mujib";
+// var lastName = "Rahman";
+
+// console.log(firstName +' '+ lastName); //10+20 == 30
+
+/*
+var mobile = {
+    height: 10,
+    width: 5,
+    color: 'black',
+    thickness: 0.5
+};
+
+console.log("My mobile's width is " + mobile["width"] +
+    "inches ,height is " + mobile.height + "inches ,color is "
+    + mobile.color + " and thickness is " + mobile.thickness);
+*/
+
+
+//parameters
+// function add(x,y) {
+//     var z=x+y;
+//     console.log(z);
+// }
+
+// add(10);
