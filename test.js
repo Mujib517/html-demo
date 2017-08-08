015b666bc26c883dafd85f2f148614b8130d46d1
Mujib517 <mujib517@gmail.com>
2017-08-08 22:11:43 +0530

    JS continued

--- test.js
+++ test.js
@@ -1,7 +1,113 @@
 
-var dt = new Date();
+var arr = [10, 20, 30, 40, 50];
 
-console.log(dt.getDate() + " - " + (dt.getMonth() + 1) + " - " + dt.getFullYear());
+for(var i=0;i<arr.length;i++){
+   console.log(arr[i]);
+}
+
+
+// var i = 0;
+// while (i < arr.length) {
+//     console.log(arr[i]);
+//    // i++;
+// }
+
+
+
+
+
+
+// var age = 50;
+
+// if (age == 20) {
+//     console.log("You are young");
+// }
+// else if (age == 30) {
+//     console.log("You are adult");
+// }
+// else if (age == 40) {
+//     console.log("You are young at heart");
+// }
+// else {
+//     console.log("You are awesome");
+// }
+
+
+// == 
+// <
+//>
+//<=
+//>=
+
+// switch (age) {
+//     case 20:  //age==20 age<=20 age>20
+//         console.log("you are young");
+//         //
+//         //
+//         //
+//         break;
+//     case 30:
+//         console.log("You are adult");
+//         break;
+//     case 40:
+//         console.log("You are young at heart");
+//         break;
+//     default:
+//         console.log("you are awesome");
+//         break;
+// }
+
+
+
+// if (age <= 20) console.log("You are young");
+// else if (age <= 30) console.log("You are adult");
+// else if (age <= 40) console.log("You are young at heart");
+// else  console.log("You are awesome");
+
+// var s = "richard first word second word third word";
+
+// var arr=s.split(' ')
+
+// console.log(arr);
+
+
+// function Add(a,b){
+//     return a+b;
+// }
+
+
+// var result=Add(2,3);
+
+// console.log(result);
+
+
+
+// console.log(s.length);
+
+// console.log(s.indexOf('word'));
+
+// console.log(s.slice(8,13));  //index: 7
+
+//console.log(s.replace("word",""));
+
+// console.log(s.substr(0, 7));
+// console.log(s.substr(8, 5));
+
+// console.log(s.toLowerCase());
+// console.log(s.toUpperCase());
+
+// var firstName = "Shawn";
+// var lastName = "Garner";
+
+// console.log(firstName.concat(' ').concat(lastName));
+// console.log(s.charAt(1)); //R
+
+
+
+
+// var dt = new Date();
+
+// console.log(dt.getDate() + " - " + (dt.getMonth() + 1) + " - " + dt.getFullYear());
 
 
 // var firstName = "Mujib";
