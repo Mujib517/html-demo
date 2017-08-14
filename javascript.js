76789dcca214b73906dbec004d8b0f8382a270df
Mujib517 <mujib517@gmail.com>
2017-08-14 22:01:14 +0530

    Javascript recap

---
+++ javascript.js
@@ -0,0 +1,101 @@
+var x;  //declaration
+
+x = 5; //initialization
+
+// var days=["Sunday","Monday","Tuesday","Wed.day","Thursday","Friday","Saturday","Good day"];
+
+// var dt=new Date();
+
+// var today=dt.getFullYear()+"-"+(dt.getMonth()+1)+"-"+dt.getDate();
+
+// console.log(today);
+
+// var flag = false;  //boolean
+
+// if (flag) {
+//     console.log('flag is set to true');
+// }
+// else {
+//     console.log('flag is set to false');
+// }
+
+function Add(a, b) {
+    return a+b;
+}
+
+var result=Add(10, 20);
+
+console.log(result);
+
+
+// Add(20, 20); 
+// Add(30,20);
+// Add();
+
+
+// var mobile = {
+//     width: 5,
+//     height: 3,
+//     color: 'red',
+
+//     call:function(){
+//         console.log('calling...');
+//     }
+// };
+
+// console.log(mobile.color);
+// console.log(mobile["color"]);
+
+// mobile.call();
+
+
+
+
+
+// //6<7 true
+// //7<7 false
+
+// for(var i=0;i<days.length;i++){
+//     console.log(days[i]);
+// }
+
+
+
+// var i=0;
+// while(i<7){
+//     console.log(days[i]);
+//     i++;
+// }
+
+//console.log('failed condition');
+
+// console.log(days[0]);
+// console.log(days[1]);
+// console.log(days[2]);
+// console.log(days[3]);
+// console.log(days[4]);
+// console.log(days[5]);
+// console.log(days[6]);
+
+
+// var day1="Sunday";
+// var day2="Monday";
+// var day3="Tuesday";
+// var day4="Wed.day";
+// var day5="Thursday";
+// var day6="Friday";
+// var day7="Saturday";
+
+// console.log(day5);
+
+
+/*
+var x=5;
+
+var name="Srinath";
+
+console.log(name);
+*/
+
+
+//console.log("Hello JS");
